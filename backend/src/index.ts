55693187e3ecb76ce777ebba10bd56b07f6c7266
Щербаков Александр Владимирович <sherbakov@arcsinus.ru>
2025-03-03 21:37:31 +0300

    added express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello!123')
+import express from 'express';
+
+const expressApp = express();
+expressApp.get('/ping', (req, res) => {
+    res.send('pong');
+})
+
+expressApp.listen(3000, () => {
+    console.log('Listening ar 3000');
+})
