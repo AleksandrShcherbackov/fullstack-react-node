1aa157b3979170e9e84d0e1cb2e61321ab978bce
Щербаков Александр Владимирович <sherbakov@arcsinus.ru>
2025-03-04 17:12:41 +0300

    test

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,4 +1,3 @@
-import viteLogo from "../../../../vite.svg"
 import reactLogo from './assets/react.svg'
 import { TrpcProvider } from './lib/trpc'
 import { AllUsersPage } from './pages/AllUsersPage'
@@ -8,9 +7,6 @@ export const App = () => {
   return (
     <TrpcProvider>
       <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
